0642beb6971dc08179fc6eca588b1771a80d71ca
Neeraja <neerups11@gmail.com>
2020-05-21 23:25:53 +0530

    Updated code for 20k inputs

--- MHK.py
+++ MHK.py
@@ -1,86 +1,119 @@
 from random import randint
 from gmpy2 import invert
+import time
 
-b = []
-w = []
-q = 0
-r = 0
+
 MAX_CHARS = 150
 BINARY_LENGTH = MAX_CHARS * 8
 
 class MHK_Crypto_w_Arrays:
-    
+
     def __init__(self):
+        self.b = []
+        self.w = []
+        self.q = 0
+        self.r = 0
         self.genKeys()
 
     def genKeys(self):
-        global w, b, q, r
         maxBits = 50
 
-        w.append(randint(1, 2**maxBits))
-        sum = w[0]
+        self.w.append(randint(1, 2**maxBits))
+        sum = self.w[0]
         for i in range(1, BINARY_LENGTH):
-            w.append(sum + randint(1, 2**maxBits))
-            sum += w[i]
+            self.w.append(sum + randint(1, 2**maxBits))
+            sum += self.w[i]
 
-        q = sum + randint(1, 2**maxBits)
-        r = q - 1
+        self.q = sum + randint(1, 2**maxBits)
+        self.r = self.q - 1
         for i in range(BINARY_LENGTH):
-            b.append((w[i]*r)%q)
+            self.b.append((self.w[i]*self.r)%self.q)
 
+    # @profile
     def encryptMsg(self, message):
-        global w, b, q, r
+
         if len(message) > MAX_CHARS:
-            raise Exception("Maximum message length allowed is " + MAX_CHARS + ".")
-        if len(message) <= 0:
-            raise Exception("Cannot encrypt an empty string.")
+                print("\nYour message should have at most ", MAX_CHARS, "characters! Please try again.\n\n")
+        elif len(message) <= 0:
+                print("\nYou message should not be empty! Please try again.\n\n")
+
 
         msgBinary = ''.join('{:08b}'.format(b) for b in message.encode('utf8'))
-        print(msgBinary)
 
         if len(msgBinary) < BINARY_LENGTH:
             msgBinary = msgBinary.zfill(BINARY_LENGTH)
 
+
         result = 0
+        
         for i in range(len(msgBinary)):
-            result += b[i]*int(msgBinary[i], 2)
+            result += self.b[i]*int(msgBinary[i],2)
 
         return str(result)
 
     def decryptMsg(self, ciphertext):
-        global w, b, q, r
-        tmp = int(ciphertext)%q*invert(r,q)%q
-        decrypted_binary = ''
 
-        for i in range(len(w)-1,-1,-1):
-            if w[i] <= tmp:
-                tmp -= w[i]
+        decrypted_binary = ''
+        ciphertext = int(ciphertext)
+
+        tmp = (ciphertext*invert(self.r,self.q))%self.q
+
+        for i in range(len(self.w)-1,-1,-1):
+            if self.w[i] <= tmp:
+                tmp -= self.w[i]
                 decrypted_binary += '1'
             else:
                 decrypted_binary += '0'
+        
+
 
         return int(decrypted_binary[::-1], 2).to_bytes((len(decrypted_binary) + 7) // 8, 'big').decode()
 
 if __name__ == "__main__":
+    
+    encrypt_time=0
+    decrypt_time=0
+    f = open("test.txt", "r")
+    messages = f.read().splitlines()
+    
+    start_keygen=time.process_time()
     crypto = MHK_Crypto_w_Arrays()
-    print("Public and private keys have been generated.\n")
-    while True:
-        print("Enter a string and I will encrypt it as single large integer:")
-        message = input()
-        if len(message) > MAX_CHARS:
-            print("\nYour message should have at most " + MAX_CHARS + "characters! Please try again.\n\n")
-        elif len(message) <= 0:
-            print("\nYou message should not be empty! Please try again.\n\n")
-        else:
-            break
+    end_keygen=time.process_time()
 
-    print("\nClear text:")
-    print(message)
-    print("\nNumber of clear text bytes = " + str(len(message)))
+    keygen_time= end_keygen-start_keygen
+ 
+    for message in messages:
+        
+ 
+       
+        while True:
+           
+            if len(message) > MAX_CHARS:
+                print("\nYour message should have at most ", MAX_CHARS, "characters! Please try again.\n\n")
+            elif len(message) <= 0:
+                print("\nYou message should not be empty! Please try again.\n\n")
+            else:
+                break
 
-    encrypted = crypto.encryptMsg(message)
-    print("\n\"" + message + "\"" + " is encrypted as:")
-    print(encrypted)
+        
+        start_encrypt = time.process_time()
+        encrypted = crypto.encryptMsg(message)
+        end_encrypt = time.process_time()
+        
+        encrypt_time += end_encrypt-start_encrypt
 
-    print("\nResult of decryption:")
-    print(crypto.decryptMsg(encrypted))
+        
+        
+        start_decrypt = time.process_time()
+        decrypted = crypto.decryptMsg(encrypted)
+        end_decrypt = time.process_time()
+        
+        decrypt_time += end_decrypt-start_decrypt
+        
+
+    f.close()
+    print("Total Time for encryption is ",encrypt_time)
+    print("Total Time for decryption is ",decrypt_time)
+    print("Total Time for key generation is ",keygen_time)
+    
+
